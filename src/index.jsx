bf7342c7670f291c32c380d2adfc692877517901
JuanCruz2 <Juan.Cruz.Gonzalez169@gmail.com>
2026-03-03 23:34:56 -0300

    day 2- jsx inject and bugs fixes

---
+++ src/index.jsx
@@ -0,0 +1,16 @@
+// 1) import the React and ReactDOM libraries   
+import React from 'react';
+import ReactDOM from 'react-dom/client'
+
+// 2) Get a reference to the div with ID root
+const el = document.getElementById('root');
+
+// 3) Tell react to tell control of that element
+const root = ReactDOM.createRoot (el)
+
+// 4) create a component 
+function App() {
+    return <h1>Hi there!</h1>;
+}
+// 5) show the component on the screen
+root.render(<App />);
